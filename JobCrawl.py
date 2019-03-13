65274ced22c04c0842406b2600c26c41d78c0bf5
Brian Orwick <Orwick12@outlook.com>
2019-03-13 19:03:41 -0400

    clean up

--- JobCrawl.py
+++ JobCrawl.py
@@ -3,10 +3,9 @@
 # Scrape Indeed for jobs in cities
 
 import requests
-from bs4 import BeautifulSoup  # need to pip install lxml
+from bs4 import BeautifulSoup  # need to pip install lxml for parsing
 from time import sleep
 import pandas as pd
-# from tkinter import Tk, Label, Button
 # from automail import email_me
 import os
 import re
@@ -31,10 +30,8 @@ for city in cities:
             soup = BeautifulSoup(page.text, 'lxml')  # from_encoding='utf-8')
             for div in soup.find_all(name='div', attrs={'class':'row'}):
                 try:
-                    # specifying row num for index of job posting in dataframe
-                    num = (len(job_df) + 1)
-                    # creating an empty list to hold the data for each posting
-                    job_post = []
+                    num = (len(job_df) + 1)  # specifying row num for index of job posting in dataframe
+                    job_post = []  # creating an empty list to hold the data for each posting
                     postLength = div.findAll('span', attrs={'class': 'date'})
                     numbers = re.findall(r'\d+', str(postLength))
                     if not numbers:
@@ -43,19 +40,15 @@ for city in cities:
                         job_post.append(numbers.pop())
                     elif int(numbers[0]) >= 30:  # if it was to far out skip the job posting
                         break
-                    # grabbing job title
-                    for b in div.find_all(name='a', attrs={'data-tn-element':'jobTitle'}):
+                    for b in div.find_all(name='a', attrs={'data-tn-element':'jobTitle'}):  # grabbing job title
                         job_post.append(b['title'])
-                    # grabbing location name
-                    a = div.findAll(attrs={'class': 'location'})
+                    a = div.findAll(attrs={'class': 'location'})  # grabbing location name
                     for span in a:
                         job_post.append(span.text)
-                    # grabbing summary text
-                    c = div.findAll('span', attrs={'class': 'summary'})
+                    c = div.findAll('span', attrs={'class': 'summary'})  # grabbing summary text
                     for span in c:
                         job_post.append(span.text.strip())
-                    # grabbing company name
-                    company = div.find_all(name='span', attrs={'class':'company'})
+                    company = div.find_all(name='span', attrs={'class':'company'})  # grabbing company name
                     if len(company) > 0:
                         for d in company:
                             job_post.append(d.text.strip())
@@ -63,8 +56,7 @@ for city in cities:
                         sec_try = div.find_all(name='span', attrs={'class':'result-link-source'})
                         for span in sec_try:
                             job_post.append(span.text)
-                    # grab link to page
-                    for link in div.find_all(name='a', attrs={'data-tn-element': 'jobTitle'}):
+                    for link in div.find_all(name='a', attrs={'data-tn-element': 'jobTitle'}):  # grab link to page
                         href = 'https://www.indeed.com' + link['href']
                         job_post.append(href)
                     job_df.loc[num] = job_post
